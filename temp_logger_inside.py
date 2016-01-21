037987aee53b592714c8d81191bb72e6ff4a4133
simona <sdalin@gmail.com>
2016-01-21 00:25:46 -0500

    Initial commit

---
+++ temp_logger_inside.py
@@ -0,0 +1,9 @@
+#Record the temperature from our raspberry pi
+tfile = open("/sys/bus/w1/devices/28-03159138f4ff/w1_slave")
+text = tfile.read()
+tfile.close()
+secondline = text.split("\n")[1]
+temperaturedata = secondline.split(" ")[9]
+temperature = float(temperaturedata[2:])
+temperature = temperature/1000
+print temperature
