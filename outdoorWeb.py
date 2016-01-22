0fb9fc5c5b82c96da679ac2adb0b1e93753320ba
Honi Sanders <honi@brandeis.edu>
2016-01-21 21:55:58 -0500

    adding outdoorWeb.py and logger.py

---
+++ outdoorWeb.py
@@ -0,0 +1,16 @@
+import requests
+import json
+from logger import Logger
+import time
+
+
+tempLog = Logger('outside_temp_log.txt')
+while True:
+    output = requests.get('http://api.wunderground.com/api/62875508aeaaee2d/conditions/q/pws:KMASOMER13.json')
+    dictionary = json.loads(output.text)
+    temperature = dictionary['current_observation']['temp_f']
+    tempLog.write(str(temperature))
+    time.sleep(5*60)
+
+
+
