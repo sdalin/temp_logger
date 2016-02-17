125b8eb295bd90e17a86f703ffbaef336ff555d1
Honi Sanders <honi@brandeis.edu>
2016-02-16 23:33:05 -0500

    outdoorHistory.py pulls historical temperatures from the union sq station on weatherunderground and puts it in outside_temp_history.txt

---
+++ outdoorHistory.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import requests
+import json
+from logger import Logger
+import time
+
+f = open('outside_temp_history.txt', 'w')
+# from 23 of january to 15 of february
+
+for month in (1, 2):
+    if month < 10:
+        month = '0' + str(month)
+    else:
+        month = str(month)
+    if month == '01':
+        days = range(23, 32)
+    elif month == '02':
+        days = range(1, 16)
+    for day in days:
+        if day < 10:
+            day = '0' + str(day)
+        else:
+            day = str(day)
+        success = False
+        tries = 0
+        while not success and tries <= 10:
+            try:
+                url = 'http://api.wunderground.com/api/62875508aeaaee2d/history_2016' + month + day + '/q/pws:KMASOMER13.json'
+                output = requests.get(url)
+                dictionary = json.loads(output.text)
+                observations = dictionary['history']['observations']
+                success = True
+            except Exception as e:
+                print str(e)
+                success = False
+                tries += 1
+        for i in xrange(len(observations)):
+            # self.log.write(time.strftime("%m/%d/%y %H:%M:%S") + " " + str(time.time()) + "\t" + message + "\n")
+            # 02/16/16 22:34:27 1455680067.13	72.275
+            date = observations[i]['date']
+            struct_time = (int(date['year']), int(date['mon']), int(date['mday']), int(date['hour']), int(date['min']), 0, 0, 0, -1)
+            unixtime = time.mktime(struct_time)
+
+            line = date['mon'] + '/' + date['mday'] + '/' + date['year'][2:4] + " " + date['hour'] + ':' + date['min']
+            line += ' ' + str(unixtime) + '\t' + observations[i]['tempi'] + '\n'
+            f.write(line)
+
+f.close()
