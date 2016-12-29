1a35936c4ea75a8540e29d5dfb04053423c44574
bringingheavendown <honi@brandeis.edu>
2016-12-29 00:06:29 -0500

    log update plus other changes on the pi

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 date
-printf "  "; tail -1 logs/temp_hum.txt
 printf "  "; tail -1 logs/inside_temp.txt
 printf "  "; tail -1 logs/outside_temp.txt
-tail nohup.out
+printf "  "; tail -3 logs/temp_hum.txt
+tail -7 nohup.out
---
+++ old/rpi-listen.cpp
@@ -0,0 +1,132 @@
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+#include <unistd.h>
+#include "RF24/RF24.h"
+
+using namespace std;
+
+/*
+ radio 7 (miso) to gpio9/bcm21
+ radio 6 (mosi) to gpio10/bcm19
+ radio 5 (sck)  to gpio11/bcm23
+ radio 4 (csn)  to gpio8/bcm24
+ radio 3 (ce)   to gpio25/bcm22
+ radio 2 (3.3v) to 3.3v
+ radio 1 (gnd)  to gnd
+ */
+
+// spi device, spi speed, ce gpio pin
+//RF24 radio("/dev/spidev0.0",8000000,25);
+RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_8MHZ);
+// D2 is base station write, everyone else read
+// E1-F2 are peripheral writes, base station read
+//const uint64_t pipes[6] = { 0xF0F0F0F0D2LL, 0xF0F0F0F0E2LL,
+//    0xF0F0F0F0E1LL, 0xF0F0F0F0E3LL,
+//    0xF0F0F0F0F1, 0xF0F0F0F0F2 };
+//uint8_t pipes[6][3] = { "0Base", "1Node", "2Node"};
+uint64_t pipes[3] = { 0xF0F0F0F0D2LL, 0xF0F0F0F0E1LL,0xF0F0F0F0E2LL};
+
+void timestring(time_t* rawtime, char* buffer, int slen)
+{
+    // get time - can be passed to time.localtime()
+    struct tm * timeinfo;
+    time(rawtime);
+    timeinfo = localtime(rawtime);
+    strftime(buffer,slen,"%m/%d/%y %H:%M:%S",timeinfo);
+}
+
+void getMessage(char* message, int len)
+{
+    char rxBuffer[len] = "";
+    radio.read(&rxBuffer, len);
+    int i;
+    for (i=0; i<len; i++) {
+        if (rxBuffer[i] == ',') {
+            message[i] = '\t';
+        } else {
+            message[i] = rxBuffer[i];
+        }
+    }
+}
+
+
+void setup(void)
+{
+    // setup radio for listening
+    radio.begin();
+    radio.setAutoAck(1);
+    radio.setRetries(15,15);
+    radio.enableDynamicPayloads();
+    radio.setDataRate(RF24_250KBPS);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setCRCLength(RF24_CRC_8);
+    radio.setChannel(76);
+    radio.openReadingPipe(1,pipes[1]);
+    radio.openReadingPipe(2,pipes[2]);
+    radio.startListening();
+    radio.printDetails();
+}
+
+void loop(void)
+{
+    uint8_t pipe_num;
+    while (radio.available(&pipe_num))
+    {
+        printf("radio available\n");
+        int slen = 32;
+        char buffer[slen] = "";
+        time_t rawtime;
+        timestring(&rawtime, buffer, slen);
+        
+        int len = radio.getDynamicPayloadSize();
+        int len2 = len;
+        
+        // read from radio
+        printf("pipe %i ",pipe_num);
+        if (pipe_num == 1)
+        {
+            printf("is pipe 1\n");
+            len2 = len;
+        } else {
+            printf("is not pipe 1\n");
+            len2 = len+4;
+        }
+        char message[len2] = "";
+        char tmp[len] = "";
+        getMessage(tmp, len);
+        int i;
+        for (i=0; i<len2-len; i++)
+        {
+            message[i] = '\t';
+        }
+        while (i<len) {
+            message[i] = tmp[i-(len2-len)];
+            i++;
+        }
+        printf(message);
+        
+        // write to file and close
+        ofstream myfile;
+        myfile.open ("/var/tmp/rf24weather2.txt");
+        myfile << buffer << ' ' << rawtime << '\t' << message << endl;
+        myfile.close();
+        myfile.open ("logs/temp_hum2.txt",ofstream::app);
+        myfile << buffer << ' ' << rawtime << '\t' << message << endl;
+        //myfile << rxBuffer << ':' << rawtime << endl;
+        myfile.close();
+    }
+            
+    // pause to reduce cpu load
+    sleep(1);
+}
+            
+int main(int argc, char** argv)
+{
+    setup();
+    while(1)
+    loop();
+                
+    return 0;
+}
