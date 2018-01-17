2749352436adb0bdced46a7493f61085ad17adae
bringingheavendown <honi@brandeis.edu>
2018-01-17 17:03:48 -0500

    errors get written to appropriate log

--- run_temp_logging.sh
+++ run_temp_logging.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-nohup python -u outdoorWeb.py &
-nohup python -u temp_logger_inside.py &
+nohup python -u outdoorWeb.py 2>>logs/outside_temp.txt &
+nohup python -u temp_logger_inside.py 2>>logs/inside_temp.txt &
 nohup sudo ./rpi-listen &
